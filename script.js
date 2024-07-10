28c1934984665f969181b9e80b01623ba7817b92
Dharani Selvan . G . C <156924833+dharaniselvan23@users.noreply.github.com>
2024-07-10 14:14:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.addEventListener('scroll', function() {
+    console.log('Scrolling detected');
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 0) {
         navbar.classList.add('scrolled');
